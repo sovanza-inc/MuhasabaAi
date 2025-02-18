19c716a68a90d57355b5a065e440dd8f2c69917a
sovanza-inc <admin@sovanza.com>
2025-02-19 04:21:38 +0500

    .

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -33,7 +33,7 @@ export default async function AppRootLayout({
   const cookieStore = await cookies()
 
   const colorMode = (cookieStore.get('chakra-ui-color-mode')?.value ??
-    'dark') as 'light' | 'dark'
+    'light') as 'light' | 'dark'
 
   return (
     <html data-theme={colorMode} style={{ colorScheme: colorMode, fontFamily: 'Lato, sans-serif' }}>
