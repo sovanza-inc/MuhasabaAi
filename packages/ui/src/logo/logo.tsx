b9b46a866b100f2a8a5fd4a51caaab17514bd5c4
sovanza-inc <admin@sovanza.com>
2025-02-19 04:39:05 +0500

    Logo Fixed

--- packages/ui/src/logo/logo.tsx
+++ packages/ui/src/logo/logo.tsx
@@ -2,7 +2,7 @@ import { FlexProps, Image, ImageProps, useColorMode } from '@chakra-ui/react'
 
 export const Logo = (props: FlexProps) => {
   const { colorMode } = useColorMode()
-  const logoSrc = colorMode === 'dark' ? '/img/onboarding/dark.svg' : '/img/onboarding/light.svg'
+  const logoSrc = colorMode === 'dark' ? '/img/onboarding/logo.svg' : '/img/onboarding/logo.svg'
   return <Image src={logoSrc} alt="Muhasaba AI" width="160px" {...props} />
 }
 
